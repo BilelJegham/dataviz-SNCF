f159e45af0081cf5cb6b72f7e56798ec9c9d8dc9
BilelJegham <contact.bileljegham@gmail.com>
2020-01-06 13:42:21 +0100

    First

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MapPerdu :data="filteredItems" :gares="gares"/>
+    <MapPerduRetrouve :data="filteredItems" :gares="gares"/>
+    <MapFre :data="filteredItems" :gares="gares"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import * as d3 from "d3"
+import MapPerdu from "./components/MapPerdu"
+import MapPerduRetrouve from "./components/MapPerduRetrouve"
+import MapFre from "./components/MapFre"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MapPerdu,
+    MapPerduRetrouve,
+    MapFre
+  },
+  data: function() {
+    return {
+      data: [],
+      gares: []
+    };
+  },  
+  created() {
+    var t = this
+    d3.csv(
+        "./table.csv"
+    ).then(function(data) {
+      t.data= data
+    });
+   d3.csv(
+        "./referentiel-gares-voyageurs.csv"
+    ).then(function(data) {
+      t.gares= data
+    });
+
+  },
+  computed: {
+      filteredItems() {
+        return this.data.filter((thing, index, self) =>
+          index === self.findIndex((t) => (
+            t.CODE_UIC === thing.CODE_UIC 
+          )))
+      }
+  
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+html,
+body, h3 {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/MapFre.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <h1>Objet Perdu Fre fonction des gares</h1>
+  <div class="body">
+   <l-map :zoom="zoom" :center="center">
+      <l-tile-layer :url="url"></l-tile-layer>
+      <l-circle 
+        v-for="(station, index) in data"
+        :lat-lng="[station.lat, station.lon]"
+        :radius="parseFloat(station.ratioPV)"
+        v-bind:key="index"
+        :color="getColor(station.CODE_UIC)"
+      ></l-circle> 
+    </l-map>
+  </div>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LCircle } from "vue2-leaflet";
+export default {
+  name: 'MapFre',
+  props: {
+    data: Array,
+    gares: Array
+  },
+  components: {
+    LMap,
+    LTileLayer,
+    LCircle
+  }, 
+  data() {
+    return {
+      zoom: 6,
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      center: [45.20,	3.20]
+    };
+  },
+  methods:{
+    getColor(codeuic){
+      let regions = {
+        "REGION ALPES":"#E69F00",
+        "REGION ALSACE":"#56B4E9",
+        "REGION AUVERGNE":"#009E73",
+        "REGION BOURGOGNE FRANCHE-COMTE":"#F0E442",
+        "REGION BRETAGNE":"#0072B2",
+        "REGION CENTRE":"#D55E00",
+        "REGION CHAMPAGNE-ARDENNE":"#CC79A7",
+        "REGION DE PARIS RIVE GAUCHE":"#E69F00",
+        "REGION DE PARIS SAINT-LAZARE":"#56B4E9",
+        "REGION DE PARIS-EST":"#009E73",
+        "REGION DE PARIS-NORD":"#F0E442",
+        "REGION DE PARIS-SUD-EST":"#0072B2",
+        "REGION LANGUEDOC-ROUSSILLON":"#D55E00",
+        "REGION LIMOUSIN":"#CC79A7",
+        "REGION LORRAINE":"#E69F00",
+        "REGION MIDI PYRENEES":"#56B4E9",
+        "REGION NORD PAS DE CALAIS":"#009E73",
+        "REGION NORMANDIE":"#F0E442",
+        "REGION PAYS DE LA LOIRE":"#0072B2",
+        "REGION PICARDIE":"#D55E00",
+        "REGION POITOU-CHARENTES AQUITAINE":"#CC79A7",
+        "REGION PROVENCE ALPES COTE D'AZUR":"#E69F00",
+        "REGION RHONE-ALPES":"#56B4E9",
+      }
+      var r = this.gares.find((g) =>{
+        if(g["Code UIC"]==codeuic){
+          return g["Région SNCF"]
+        }
+      });
+      if(r)
+        return regions[r["Région SNCF"]]
+      else  
+        return "white"
+    }
+
+  
+  }
+}
+</script>
+
+<style scoped>
+.body {
+  width: 700px;
+  height: 625px;
+}
+</style>
+
